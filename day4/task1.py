c139c2b57b66263ee994f90f1aed2521f15c9a05
Paulo Henrique Silva Pinto <paulohsilvapinto@gmail.com>
2023-12-09 17:14:00 +0000

    Add day 4

---
+++ day4/task1.py
@@ -0,0 +1,22 @@
+from commons.utils import read_input
+import sys
+
+print(sys.path)
+from commons.testa import hello
+import re
+
+games = read_input(day_number=4)
+response = 0
+
+for game in games:
+    winning_numbers = re.findall(r"\:\s+(.*)\s+\|", game)[0].split()
+    scratched_numbers = re.findall(r"\|\s+(.*)", game)[0].split()
+
+    count_correct_numbers = 0
+    for number in scratched_numbers:
+        if number in winning_numbers:
+            count_correct_numbers += 1
+    
+    response += (2 ** (count_correct_numbers - 1) if count_correct_numbers else 0)
+
+print(response)
